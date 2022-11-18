ae4bc0850e1dcf08e9e93a14f9c16622d5d39151
bibiba <vyckluchi75@gmail.com>
2022-11-18 04:10:07 +0300

    add backend Dockerfile & update docker-compose.yml

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,4 +5,4 @@ Run application.
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run('app:app', host='127.0.0.1', port=8080, reload=True)
+    uvicorn.run('app:app', host='127.0.0.1', port=5000, reload=True)
